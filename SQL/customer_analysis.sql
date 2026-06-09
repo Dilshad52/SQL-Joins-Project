61780f8cdeec79eceb188c82f9775448f614d5e2
Mohammed Dilshad <dilshadmohammed547@gmail.com>
2026-06-09 21:19:55 +0530

    Update customer_analysis.sql

--- SQL/customer_analysis.sql
+++ SQL/customer_analysis.sql
@@ -1,16 +1,19 @@
 -- Customers Without Orders
 
-SELECT c.customer_id,
-       c.customer_name
-FROM customers c
-LEFT JOIN orders o
-ON c.customer_id = o.customer_id
-WHERE o.customer_id IS NULL;
+SELECT
+c.Customer_Name
+FROM Customers c
+LEFT JOIN Orders o
+ON c.Customer_ID = o.Customer_ID
+WHERE o.Customer_ID IS NULL;
 
 -- Top Customers
 
-SELECT customer_name,
-       SUM(revenue) AS Total_Revenue
-FROM orders
-GROUP BY customer_name
-ORDER BY Total_Revenue DESC;
+SELECT
+c.Customer_Name,
+SUM(o.Revenue) AS Total_Spent
+FROM Customers c
+JOIN Orders o
+ON c.Customer_ID = o.Customer_ID
+GROUP BY c.Customer_Name
+ORDER BY Total_Spent DESC;
